ae3c77cf8f2eb34169eede97cd8f6b1cfd5bc476
HOL Support <support@hol.org>
2026-08-21 15:04:50 -0400

    feat: add CrewAI HOL Guard pre-tool hook adapter

---
+++ integrations/crewai-hol-guard/src/crewai_hol_guard/__init__.py
@@ -0,0 +1,17 @@
+from .hook import (
+    GuardDecision,
+    HolGuardCrewAIHook,
+    HolGuardUnavailable,
+    enable_hol_guard,
+    evaluate_with_hol_guard,
+    interactive_approval,
+)
+
+__all__ = [
+    "GuardDecision",
+    "HolGuardCrewAIHook",
+    "HolGuardUnavailable",
+    "enable_hol_guard",
+    "evaluate_with_hol_guard",
+    "interactive_approval",
+]
